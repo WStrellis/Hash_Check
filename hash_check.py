044a1d63f1b0d1ad9f62b67c06bc7b68f0920bed
WStrellis <wstrellis@gmail.com>
2018-05-11 10:42:11 -0400

    Added hash_check.py and its test module test_hash_check.py

---
+++ hash_check.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+# A program for storing passwords
+
+import sys 
+import hashlib
+algorithms = ['sha1()','sha224()','sha256()','sha384()','sha512()', \
+		      'blake2b()','blake2s()','md5()']
+if len(sys.argv) < 4:
+	print('Usage: hash_check.py [algorithm] [file to hash] [correct hash]\n\
+    \rUse [algorithm] to create a hash for [file to hash]. Compare the result\
+ to [correct hash].\n \
+	 \rSupported algorithm arguments:\n\
+	 \r%s' %(' '.join(algorithms)))
+	# print(algorithms,end=None)
+	
+
+	# sys.exit()
+
+# account = sys.argv[1].lower() # first command line arg is the account name
+
+# if account in passwords:
+	# print('Password for {} is: {}'.format(account,passwords.get(account)))
+# else:
+	# print("There is no account named %s" %(account))
+print(hashlib.md5(b"hello").hexdigest())
+#testing git
+#another git test
---
+++ test_hash_check.py
@@ -0,0 +1,6 @@
+import unittest
+
+class TestAlgorithem(unittest.TestCase):
+    """ Tests to see if the algorithem is correctly formatted"""
+
+    def test
