c07a650796cbdf7bb4cc70592baee8b81ab5ba4c
WStrellis <wstrellis@gmail.com>
2018-05-11 15:00:38 -0400

    Added an 'or' statement to line 9

--- hash_check.py
+++ hash_check.py
@@ -6,7 +6,7 @@ import sys
 import hashlib
 algorithms = ['sha1()','sha224()','sha256()','sha384()','sha512()', \
 		      'blake2b()','blake2s()','md5()']
-if len(sys.argv) < 4:
+if len(sys.argv) < 4 or sys.argv[1] not in algorithms:
 	print('Usage: hash_check.py [algorithm] [file to hash] [correct hash]\n\
     \rUse [algorithm] to create a hash for [file to hash]. Compare the result\
  to [correct hash].\n \
