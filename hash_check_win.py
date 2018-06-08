cbead7d81bccf2273fdfc3678b8b34adaf44332a
WStrellis <wstrellis@gmail.com>
2018-06-08 13:27:44 -0400

    stuff

--- hash_check_win.py
+++ hash_check_win.py
@@ -17,10 +17,4 @@ if len(sys.argv) < 4 or sys.argv[1] not in algorithms:
 
 	# sys.exit()
 
-# account = sys.argv[1].lower() # first command line arg is the account name
-
-# if account in passwords:
-	# print('Password for {} is: {}'.format(account,passwords.get(account)))
-# else:
-	# print("There is no account named %s" %(account))
 print(hashlib.md5(b"hello").hexdigest())
